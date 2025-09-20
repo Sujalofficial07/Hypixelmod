5a2600ae00140e701c7ac060eb8c832017b97485
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-20 13:45:04 +0530

    Update Island.java

--- src/main/java/com/skyblockmod/islands/Island.java
+++ src/main/java/com/skyblockmod/islands/Island.java
@@ -5,19 +5,15 @@ import java.util.UUID;
 public class Island {
     private final UUID owner;
     private String name;
-    private int level;
+    private int level = 1;
 
     public Island(UUID owner, String name) {
         this.owner = owner;
         this.name = name;
-        this.level = 1;
     }
 
     public UUID getOwner() { return owner; }
     public String getName() { return name; }
     public int getLevel() { return level; }
-
-    public void upgrade() {
-        level++;
-    }
+    public void upgrade() { level++; }
 }
